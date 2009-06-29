509dbd79544a585d17c45dc1b419c52a99a57319
Shashank Deshpande <shashankguru@gmail.com>
2009-06-29 07:06:27 +0000

    Removing Filenet 4.0 connector project from 1.3.x branch.

--- projects_v4/fnapi-client/source/java/com/google/enterprise/connector/file/filewrap/ICredentialMap.java
+++
@@ -1,8 +0,0 @@
-package com.google.enterprise.connector.file.filewrap;
-
-public interface ICredentialMap {
-	
-	public void putUserCred(String name, String password);
-	
-	public String getUserCred(String name);
-}
