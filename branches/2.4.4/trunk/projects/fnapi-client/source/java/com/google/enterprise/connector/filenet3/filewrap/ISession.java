ccf886a1d732f76e2c02e57ab0d5df5144f08b85
pankajchohan@gmail.com <pankajchohan@gmail.com@66051e46-9123-0410-adf3-a3a6f5f9a7af>
2010-03-12 13:31:02 +0000

    Copy from Trunk to Branch for 2.4.4 RC1

---
+++ branches/2.4.4/trunk/projects/fnapi-client/source/java/com/google/enterprise/connector/filenet3/filewrap/ISession.java
@@ -0,0 +1,16 @@
+package com.google.enterprise.connector.filenet3.filewrap;
+
+import java.io.FileInputStream;
+
+import com.filenet.wcm.api.Session;
+import com.google.enterprise.connector.spi.RepositoryLoginException;
+import com.google.enterprise.connector.spi.RepositoryException;
+
+public interface ISession {
+
+	public IUser verify() throws RepositoryLoginException, RepositoryException;
+
+	public void setConfiguration(FileInputStream stream);
+
+	public Session getSession();
+}
---
+++ branches/2.4.4/trunk/projects_v4/fnapi-client/source/java/com/google/enterprise/connector/filenet4/FileDateValue.java
@@ -0,0 +1,81 @@
+package com.google.enterprise.connector.filenet4;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import com.google.enterprise.connector.spi.Value;
+//import com.google.enterprise.connector.spiimpl.DateValue;
+import com.google.enterprise.connector.spiimpl.ValueImpl;
+
+public class FileDateValue extends ValueImpl {
+
+	Calendar calendarValue;
+
+	public FileDateValue(Calendar calendarValue) {
+		this.calendarValue = calendarValue;
+	}
+
+	public String toFeedXml() {
+		return toString();
+	}
+
+	public String toRfc822() {
+		return Value.calendarToRfc822(calendarValue);
+	}
+
+	public String toIso8601() {
+		return FileDateValue.calendarToIso8601(calendarValue);
+	}
+
+	public String toString() {
+		return toIso8601();
+	}
+
+	public boolean toBoolean() {
+		return (calendarValue == null);
+	}
+
+	public Calendar getCalendarValue() {
+		return this.calendarValue;
+	}
+
+	private static final SimpleDateFormat ISO8601_DATE_FORMAT_MILLIS = new SimpleDateFormat(
+			"yyyy-MM-dd'T'HH:mm:ss.SSS");
+
+	private static final SimpleDateFormat ISO8601_DATE_FORMAT_SECS = new SimpleDateFormat(
+			"yyyy-MM-dd'T'HH:mm:ss");
+
+	private static synchronized Date iso8601ToDate(String s)
+			throws ParseException {
+		Date d = null;
+		try {
+			d = ISO8601_DATE_FORMAT_MILLIS.parse(s);
+			return d;
+		} catch (ParseException e) {
+			// this is just here so we can try another format
+		}
+		d = ISO8601_DATE_FORMAT_SECS.parse(s);
+		return d;
+	}
+
+	public static synchronized Calendar iso8601ToCalendar(String s)
+			throws ParseException {
+		Date d = iso8601ToDate(s);
+		Calendar c = Calendar.getInstance();
+		c.setTime(d);
+		return c;
+	}
+
+	public static synchronized String calendarToIso8601(Calendar c) {
+		Date d = c.getTime();
+		String isoString = ISO8601_DATE_FORMAT_MILLIS.format(d);
+		return isoString;
+	}
+
+	public String FiletoIso8601() {
+		return FileDateValue.calendarToIso8601(calendarValue);
+	}
+
+}
