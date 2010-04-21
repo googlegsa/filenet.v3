743b7c56d341dbd730b1f870316c42b984a2af6a
rakeshs101981 <rakeshs101981@66051e46-9123-0410-adf3-a3a6f5f9a7af>
2010-04-21 08:24:14 +0000

    Formatting changes

--- trunk/projects_v4/fnapi-client/source/java/com/google/enterprise/connector/filenet4/FileDateValue.java
+++ trunk/projects_v4/fnapi-client/source/java/com/google/enterprise/connector/filenet4/FileDateValue.java
@@ -1,5 +1,8 @@
 package com.google.enterprise.connector.filenet4;
 
+import com.google.enterprise.connector.spi.Value;
+import com.google.enterprise.connector.spiimpl.ValueImpl;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -8,107 +11,104 @@ import java.util.TimeZone;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.google.enterprise.connector.spi.Value;
-import com.google.enterprise.connector.spiimpl.ValueImpl;
-
 public class FileDateValue extends ValueImpl {
 
-	private static Logger logger = null;
-	{
-		logger = Logger.getLogger(FileDateValue.class.getName());
-	}
+    private static Logger logger = null;
+    {
+        logger = Logger.getLogger(FileDateValue.class.getName());
+    }
 
-	Calendar calendarValue;
+    Calendar calendarValue;
 
-	public FileDateValue() {
+    public FileDateValue() {
 
-	}
+    }
 
-	public FileDateValue(Calendar calendarValue) {
-		this.calendarValue = calendarValue;
-	}
+    public FileDateValue(Calendar calendarValue) {
+        this.calendarValue = calendarValue;
+    }
 
-	public String toFeedXml() {
-		return toString();
-	}
+    public String toFeedXml() {
+        return toString();
+    }
 
-	public String toRfc822() {
-		return Value.calendarToRfc822(calendarValue);
-	}
+    public String toRfc822() {
+        return Value.calendarToRfc822(calendarValue);
+    }
 
-	public String toIso8601() {
-		return FileDateValue.calendarToIso8601(calendarValue);
-	}
+    public String toIso8601() {
+        return FileDateValue.calendarToIso8601(calendarValue);
+    }
 
-	public String toString() {
-		return toIso8601();
-	}
+    public String toString() {
+        return toIso8601();
+    }
 
-	public boolean toBoolean() {
-		return (calendarValue == null);
-	}
+    public boolean toBoolean() {
+        return (calendarValue == null);
+    }
 
-	public Calendar getCalendarValue() {
-		return this.calendarValue;
-	}
+    public Calendar getCalendarValue() {
+        return this.calendarValue;
+    }
 
-	private static final TimeZone TIME_ZONE_GMT = TimeZone.getTimeZone("GMT+0");
-	private static final Calendar CALENDAR = Calendar.getInstance();
-	private static final Calendar GMT_CALENDAR = Calendar.getInstance(TIME_ZONE_GMT);
+    private static final TimeZone TIME_ZONE_GMT = TimeZone.getTimeZone("GMT+0");
+    private static final Calendar CALENDAR = Calendar.getInstance();
+    private static final Calendar GMT_CALENDAR = Calendar.getInstance(TIME_ZONE_GMT);
 
-	private static final SimpleDateFormat ISO8601_DATE_FORMAT_MILLIS = new SimpleDateFormat(
-			"yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+    private static final SimpleDateFormat ISO8601_DATE_FORMAT_MILLIS = new SimpleDateFormat(
+            "yyyy-MM-dd'T'HH:mm:ss.SSSZ");
 
-	private static final SimpleDateFormat ISO8601_DATE_FORMAT_SECS = new SimpleDateFormat(
-			"yyyy-MM-dd'T'HH:mm:ssZ");
+    private static final SimpleDateFormat ISO8601_DATE_FORMAT_SECS = new SimpleDateFormat(
+            "yyyy-MM-dd'T'HH:mm:ssZ");
 
-	// Set the date formatter to GMT+0 calendar, so that dates used for
-	// checkpoint are always in GMT+0 i.e. UTC
-	static {
-		ISO8601_DATE_FORMAT_MILLIS.setCalendar(GMT_CALENDAR);
-		ISO8601_DATE_FORMAT_SECS.setCalendar(GMT_CALENDAR);
-	}
+    // Set the date formatter to GMT+0 calendar, so that dates used for
+    // checkpoint are always in GMT+0 i.e. UTC
+    static {
+        ISO8601_DATE_FORMAT_MILLIS.setCalendar(GMT_CALENDAR);
+        ISO8601_DATE_FORMAT_SECS.setCalendar(GMT_CALENDAR);
+    }
 
-	private static synchronized Date iso8601ToDate(String s)
-			throws ParseException {
-		Date d = null;
-		try {
-			d = ISO8601_DATE_FORMAT_MILLIS.parse(s);
-			return d;
-		} catch (ParseException e) {
-			logger.log(Level.WARNING, "Unable to parse date in milli-second format yyyy-MM-dd'T'HH:mm:ss.SSSZ. Trying to parse in second format yyyy-MM-dd'T'HH:mm:ssZ");
-		}
-		d = ISO8601_DATE_FORMAT_SECS.parse(s);
-		return d;
-	}
+    private static synchronized Date iso8601ToDate(String s)
+            throws ParseException {
+        Date d = null;
+        try {
+            d = ISO8601_DATE_FORMAT_MILLIS.parse(s);
+            return d;
+        } catch (ParseException e) {
+            logger.log(Level.WARNING, "Unable to parse date in milli-second format yyyy-MM-dd'T'HH:mm:ss.SSSZ. Trying to parse in second format yyyy-MM-dd'T'HH:mm:ssZ");
+        }
+        d = ISO8601_DATE_FORMAT_SECS.parse(s);
+        return d;
+    }
 
-	public static synchronized String calendarToIso8601(Calendar c) {
-		Date d = c.getTime();
-		String isoString = ISO8601_DATE_FORMAT_MILLIS.format(d);
-		// Since the date string returned in in the form
-		// yyyy-MM-dd'T'HH:mm:ss.SSS+0000
-		// This may lead to error when actual TimeZone will be concatenated with
-		// this date.
-		// Thus removing the last substring "+0000"
-		isoString = isoString.replaceFirst("\\+0000", "");
-		return isoString;
-	}
+    public static synchronized String calendarToIso8601(Calendar c) {
+        Date d = c.getTime();
+        String isoString = ISO8601_DATE_FORMAT_MILLIS.format(d);
+        // Since the date string returned in in the form
+        // yyyy-MM-dd'T'HH:mm:ss.SSS+0000
+        // This may lead to error when actual TimeZone will be concatenated with
+        // this date.
+        // Thus removing the last substring "+0000"
+        isoString = isoString.replaceFirst("\\+0000", "");
+        return isoString;
+    }
 
-	public static synchronized String calendarToIso8601(String s)
-			throws ParseException {
-		Date d = iso8601ToDate(s);
-		String isoString = ISO8601_DATE_FORMAT_MILLIS.format(d);
-		// Since the date string returned in in the form
-		// yyyy-MM-dd'T'HH:mm:ss.SSS+0000
-		// This may lead to error when actual TimeZone will be concatenated with
-		// this date.
-		// Thus removing the last substring "+0000"
-		isoString = isoString.replaceFirst("\\+0000", "");
-		return isoString;
-	}
+    public static synchronized String calendarToIso8601(String s)
+            throws ParseException {
+        Date d = iso8601ToDate(s);
+        String isoString = ISO8601_DATE_FORMAT_MILLIS.format(d);
+        // Since the date string returned in in the form
+        // yyyy-MM-dd'T'HH:mm:ss.SSS+0000
+        // This may lead to error when actual TimeZone will be concatenated with
+        // this date.
+        // Thus removing the last substring "+0000"
+        isoString = isoString.replaceFirst("\\+0000", "");
+        return isoString;
+    }
 
-	public String FiletoIso8601() {
-		return FileDateValue.calendarToIso8601(calendarValue);
-	}
+    public String FiletoIso8601() {
+        return FileDateValue.calendarToIso8601(calendarValue);
+    }
 
 }
