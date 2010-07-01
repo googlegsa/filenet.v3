1db0c4ea4c4b8aef13906fd6d5cbc119a918fc64
aniladhe@gmail.com <aniladhe@gmail.com@66051e46-9123-0410-adf3-a3a6f5f9a7af>
2010-07-01 12:55:36 +0000

    Tagging 2.6.0_RC2 branch as 2.6.0

---
+++ tags/2.6.0/projects/fnapi-core/source/javatests/com/google/enterprise/connector/filenet3/FileConnectorTest.java
@@ -0,0 +1,40 @@
+package com.google.enterprise.connector.filenet3;
+
+import com.google.enterprise.connector.filenet3.FileConnector;
+import com.google.enterprise.connector.filenet3.FileSession;
+import com.google.enterprise.connector.spi.Connector;
+import com.google.enterprise.connector.spi.RepositoryLoginException;
+import com.google.enterprise.connector.spi.RepositoryException;
+import com.google.enterprise.connector.spi.Session;
+
+import junit.framework.TestCase;
+
+public class FileConnectorTest extends TestCase {
+
+	protected void setUp() throws Exception {
+	}
+
+	/*
+	 * Test method for
+	 * 'com.google.enterprise.connector.file.FileConnector.login()'
+	 */
+	public void testLogin() throws RepositoryLoginException,
+			RepositoryException {
+		Connector connector = new FileConnector();
+		((FileConnector) connector).setUsername(FnConnection.userName);
+		((FileConnector) connector).setPassword(FnConnection.password);
+		((FileConnector) connector)
+				.setObject_store(FnConnection.objectStoreName);
+		((FileConnector) connector)
+				.setWorkplace_display_url(FnConnection.displayUrl);
+		((FileConnector) connector)
+				.setObject_factory(FnConnection.objectFactory);
+		((FileConnector) connector)
+				.setPath_to_WcmApiConfig(FnConnection.pathToWcmApiConfig);
+		((FileConnector) connector).setIs_public("false");
+		Session sess = connector.login();
+		assertNotNull(sess);
+		assertTrue(sess instanceof FileSession);
+	}
+
+}
