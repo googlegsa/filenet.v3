0ab8cd382a1e727c67f6d0cbde993ae232d780f3
ddhanashri@googlemail.com <ddhanashri@googlemail.com@66051e46-9123-0410-adf3-a3a6f5f9a7af>
2011-05-05 09:24:40 +0000

    Creating branch for - Additional delete clause FileNet 3.5 connector configuration page

---
+++ changes/Dhanashri/FileNet 2.8.0 Changes/Additional_delete_35/fnapi-client/source/javatests/com/google/enterprise/connector/filenet3/filemockwrap/MockFnProperties.java
@@ -0,0 +1,38 @@
+package com.google.enterprise.connector.filenet3.filemockwrap;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+import com.google.enterprise.connector.filenet3.filewrap.IProperties;
+import com.google.enterprise.connector.filenet3.filewrap.IProperty;
+import com.google.enterprise.connector.mock.MockRepositoryProperty;
+import com.google.enterprise.connector.mock.MockRepositoryPropertyList;
+
+public class MockFnProperties implements IProperties {
+
+	Vector mrPL;
+
+	int size = 0;
+
+	protected MockFnProperties(MockRepositoryPropertyList propLst) {
+		mrPL = new Vector();
+		Iterator it = propLst.iterator();
+		while (it.hasNext()) {
+			this.mrPL.add(it.next());
+			size++;
+		}
+	}
+
+	public IProperty get(int index) {
+		return new MockFnProperty((MockRepositoryProperty) mrPL.get(index));
+	}
+
+	public Iterator iterator() {
+		return this.mrPL.iterator();
+	}
+
+	public int size() {
+		return this.size;
+	}
+
+}
