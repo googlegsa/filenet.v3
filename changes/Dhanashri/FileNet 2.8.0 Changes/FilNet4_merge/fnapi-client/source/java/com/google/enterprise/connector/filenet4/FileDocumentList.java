3b1e912697b79b62df605a2798ad81f98a96043b
ddhanashri@googlemail.com <ddhanashri@googlemail.com@66051e46-9123-0410-adf3-a3a6f5f9a7af>
2011-06-08 06:25:37 +0000

    Committing changes for FileNet4 2.8 release code merge(Integration) : Issues Fixed: 1.	[Issue 4445625] : Remove 'Make Public' Check box from connector Configuration page. 2.	[Issue 4444687] : FileNet connector should support 'Marking sets' for access control. 3.	[Issue 4450001] : FileNet4.0,4.5: Save configuration with Additional Where clause throws exception in logs and stops crawling. 4.	[Issue 4445640] : Error messages are displayed in English on Connector Configuration page in GSA even when we change the browser language. 5.	[Issue 4352584] : Authorization failing due comparison criterion 6.	[Issue 4348956] : To Provide 'Additional Delete Clause' on connector configuration page for sending delete feed for documents matching particular condition. 7.	[Issue 4445643] : Provision to set the ID comparison criteria for change detection. 8.	[Issue 4444700] : Crawling specific subclass of the document class.

--- changes/Dhanashri/FileNet 2.8.0 Changes/FilNet4_merge/fnapi-client/source/java/com/google/enterprise/connector/filenet4/FileDocumentList.java
+++ changes/Dhanashri/FileNet 2.8.0 Changes/FilNet4_merge/fnapi-client/source/java/com/google/enterprise/connector/filenet4/FileDocumentList.java
@@ -1,16 +1,5 @@
 package com.google.enterprise.connector.filenet4;
 
-import java.text.ParseException;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import com.google.enterprise.connector.filenet4.filewrap.IBaseObject;
 import com.google.enterprise.connector.filenet4.filewrap.IObjectSet;
 import com.google.enterprise.connector.filenet4.filewrap.IObjectStore;
@@ -20,21 +9,36 @@ import com.google.enterprise.connector.spi.RepositoryDocumentException;
 import com.google.enterprise.connector.spi.RepositoryException;
 import com.google.enterprise.connector.spi.SpiConstants;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class FileDocumentList implements DocumentList {
 
 	private static final long serialVersionUID = 1L;
 	private FileDocument fileDocumentToDelete = null;
+	private FileDocument fileDocumentToDeleteDocs = null;
 	private IObjectStore objectStore = null;
 	private IObjectSet objectSet = null;
 	private IObjectSet objectSetToDelete = null;
+	private IObjectSet objectSetToDeleteDocs = null;
 	private FileDocument fileDocument = null;
 	private Iterator ObjectIt = null;
 	private Iterator ObjectItToDelete = null;
+	private Iterator ObjectItToDeleteDocs = null;
 	private String docId = "";
 	private String displayUrl;
 	private String lastCheckPoint;
 	private String dateFirstPush;
 	private String docIdToDelete = "";
+	private String docIdToDeleteDocs = "";
 	private boolean isPublic;
 	private HashSet included_meta;
 	private HashSet excluded_meta;
@@ -43,11 +47,13 @@ public class FileDocumentList implements DocumentList {
 	// private NodeList dataToDelete = null;
 	private static Logger logger = Logger.getLogger(FileDocumentList.class.getName());
 
-	public FileDocumentList(IObjectSet objectSet, IObjectSet objectSetToDelete,
+	public FileDocumentList(IObjectSet objectSet,
+			IObjectSet objectSetToDeleteDocs, IObjectSet objectSetToDelete,
 			IObjectStore objectStore, boolean isPublic, String displayUrl,
 			HashSet included_meta, HashSet excluded_meta, String dateFirstPush,
 			String checkPoint) {
 		this.objectSet = objectSet;
+		this.objectSetToDeleteDocs = objectSetToDeleteDocs;
 		this.objectSetToDelete = objectSetToDelete;
 		this.objectStore = objectStore;
 		this.isPublic = isPublic;
@@ -60,6 +66,7 @@ public class FileDocumentList implements DocumentList {
 		this.index = 0;
 		this.ObjectIt = objectSet.getIterator();
 		this.ObjectItToDelete = objectSetToDelete.getIterator();
+		this.ObjectItToDeleteDocs = objectSetToDeleteDocs.getIterator();
 
 		// Docs to Add
 		logger.log(Level.INFO, "Number of new documents discovered: "
@@ -68,19 +75,70 @@ public class FileDocumentList implements DocumentList {
 		// Docs to Delete
 		logger.log(Level.INFO, "Number of new documents to be removed: "
 				+ this.objectSetToDelete.getSize());
+
+		logger.log(Level.INFO, "Number of new documents to be removed: (matching delete where clause) "
+				+ this.objectSetToDeleteDocs.getSize());
+
 	}
 
+	public FileDocumentList(IObjectSet objectSet, IObjectSet objectSetToDelete,
+			IObjectStore objectStore, boolean isPublic, String displayUrl,
+			HashSet included_meta, HashSet excluded_meta, String dateFirstPush,
+			String checkPoint) {
+		this.objectSet = objectSet;
+		this.objectSetToDelete = objectSetToDelete;
+		this.objectSetToDeleteDocs = null;
+		this.objectStore = objectStore;
+		this.isPublic = isPublic;
+		this.displayUrl = displayUrl;
+		this.included_meta = included_meta;
+		this.excluded_meta = excluded_meta;
+		this.dateFirstPush = dateFirstPush;
+		this.lastCheckPoint = checkPoint;
+
+		this.index = 0;
+		this.ObjectIt = objectSet.getIterator();
+		this.ObjectItToDelete = objectSetToDelete.getIterator();
+		this.ObjectItToDeleteDocs = null;
+
+		// Docs to Add
+		logger.log(Level.INFO, "Number of new documents discovered: "
+				+ this.objectSet.getSize());
+
+		// Docs to Delete
+		logger.log(Level.INFO, "Number of new documents to be removed: "
+				+ this.objectSetToDelete.getSize());
+
+	}
+
+	/***
+	 * The nextDocument method gets the next document from the document list
+	 * that the connector acquires from the FileNet repository.
+	 * 
+	 * @param
+	 * @return com.google.enterprise.connector.spi.Document
+	 */
 	public com.google.enterprise.connector.spi.Document nextDocument()
 			throws RepositoryDocumentException {
 		logger.entering("FileDocumentList", "nextDocument()");
 
 		int dataLen = this.objectSet.getSize();
+		int dataLenToDeleteDocs = 0;
+		if (this.objectSetToDeleteDocs != null) {
+			dataLenToDeleteDocs = this.objectSetToDeleteDocs.getSize();
+		} else {
+			dataLenToDeleteDocs = 0;
+		}
+
 		int dataLenToDelete = this.objectSetToDelete.getSize();
 
 		logger.log(Level.FINE, "Number of documents to be retrieved: "
 				+ dataLen);
+		logger.log(Level.FINE, "Number of indexes to be deleted from appliance (Matching Delete where clause): "
+				+ dataLenToDeleteDocs);
 		logger.log(Level.FINE, "Number of indexes to be deleted from appliance: "
 				+ dataLenToDelete);
+
 		logger.log(Level.FINE, "Index of the document in list " + index);
 
 		if (index > -1 && index < dataLen) {
@@ -91,35 +149,79 @@ public class FileDocumentList implements DocumentList {
 				docId = doc.getId(SpiConstants.ActionType.ADD);
 				Date dateLastModified = doc.getModifyDate(SpiConstants.ActionType.ADD);
 
-				fileDocument = new FileDocument(docId, dateLastModified,
-						this.objectStore, this.isPublic, this.displayUrl,
-						this.included_meta, this.excluded_meta,
-						SpiConstants.ActionType.ADD);
-				index++;
-				return fileDocument;
+				if (((docId != null) && !(docId.equals("")))
+						&& ((dateLastModified != null))) {
+
+					fileDocument = new FileDocument(docId, dateLastModified,
+							this.objectStore, this.isPublic, this.displayUrl,
+							this.included_meta, this.excluded_meta,
+							SpiConstants.ActionType.ADD);
+					index++;
+					return fileDocument;
+				} else {
+					index++;
+				}
 			}
-		} else if ((index >= dataLen) && index < (dataLenToDelete + dataLen)) {
+		} else if ((index >= dataLen)
+				&& index < (dataLenToDeleteDocs + dataLen)) {
+			logger.info("DEL...");
+			if (this.ObjectItToDeleteDocs.hasNext()) {
+				IBaseObject doc = (IBaseObject) this.ObjectItToDeleteDocs.next();
+				docIdToDeleteDocs = doc.getId(SpiConstants.ActionType.ADD);
+				Date dateLastModified = doc.getModifyDate(SpiConstants.ActionType.ADD);
+				String commonVersionId = doc.getVersionSeriesId(SpiConstants.ActionType.ADD);
+				docIdToDeleteDocs = "{" + docIdToDeleteDocs + "}";
+				commonVersionId = "{" + commonVersionId + "}";
+
+				if (((docIdToDeleteDocs != null) && !(docIdToDeleteDocs.equals("")))
+						&& ((dateLastModified != null))
+						&& ((commonVersionId != null) && !(commonVersionId.equals("")))) {
+					fileDocumentToDeleteDocs = new FileDocument(
+							docIdToDeleteDocs, commonVersionId,
+							dateLastModified, this.objectStore, this.isPublic,
+							this.displayUrl, this.included_meta,
+							this.excluded_meta, SpiConstants.ActionType.DELETE);
+					index++;
+					return fileDocumentToDeleteDocs;
+				} else {
+					index++;
+				}
+			}
+
+		} else if ((index >= dataLenToDeleteDocs + dataLen)
+				&& index < (dataLenToDelete + dataLenToDeleteDocs + dataLen)) {
 			logger.info("DEL...");
 			if (this.ObjectItToDelete.hasNext()) {
 				IBaseObject doc = (IBaseObject) this.ObjectItToDelete.next();
 				if (doc.getClassNameEvent().contains("DeletionEvent")) {
-					docId = doc.getId(SpiConstants.ActionType.DELETE);
+
+					docIdToDelete = doc.getId(SpiConstants.ActionType.DELETE);
 					Date dateLastModified = doc.getModifyDate(SpiConstants.ActionType.DELETE);
-					String commonVersionId = doc.getVersionSeriesId();
+					String commonVersionId = doc.getVersionSeriesId(SpiConstants.ActionType.DELETE);
 
 					// Since the record url sent to GSA contains "{" and "}" pre
 					// and post of the commonVersionId
 					// we need to append braces so that indexed document can get
 					// deleted from the appliance.
-					docId = "{" + docId + "}";
+					docIdToDelete = "{" + docIdToDelete + "}";
 					commonVersionId = "{" + commonVersionId + "}";
-					fileDocumentToDelete = new FileDocument(docId,
-							commonVersionId, dateLastModified,
-							this.objectStore, this.isPublic, this.displayUrl,
-							this.included_meta, this.excluded_meta,
-							SpiConstants.ActionType.DELETE);
-					index++;
-					return fileDocumentToDelete;
+
+					if (((docIdToDelete != null) && !(docIdToDelete.equals("")))
+							&& ((dateLastModified != null))
+							&& ((commonVersionId != null) && !(commonVersionId.equals("")))) {
+
+						fileDocumentToDelete = new FileDocument(docIdToDelete,
+								commonVersionId, dateLastModified,
+								this.objectStore, this.isPublic,
+								this.displayUrl, this.included_meta,
+								this.excluded_meta,
+								SpiConstants.ActionType.DELETE);
+
+						index++;
+						return fileDocumentToDelete;
+					} else {
+						index++;
+					}
 				}
 			}
 		}
@@ -128,10 +230,23 @@ public class FileDocumentList implements DocumentList {
 		return null;
 	}
 
+	/***
+	 * Checkpoint method indicates the current position within the document
+	 * list, that is where to start a resumeTraversal method. The checkpoint
+	 * method returns information that allows the resumeTraversal method to
+	 * resume on the document that would have been returned by the next call to
+	 * the nextDocument method.
+	 * 
+	 * @param
+	 * @return String checkPoint - information that allows the resumeTraversal
+	 *         method to resume on the document
+	 */
 	public String checkpoint() throws RepositoryException {
+
 		logger.log(Level.FINE, "Creation of the Checkpoint");
 		String dateString = "";
 		String dateStringDocumentToDelete = "";
+		String dateStringDocumentToDeleteDocs = "";
 		logger.info("fileDocument : " + fileDocument);
 
 		if (fileDocument != null) {
@@ -150,7 +265,8 @@ public class FileDocumentList implements DocumentList {
 			logger.log(Level.FINE, "dateString of the checkpoint of added document is "
 					+ dateString);
 
-		} else {
+		} else if (lastCheckPoint != null) {
+			logger.fine("Get the last modified date from the last checkpoint ");
 			JSONObject jo;
 			try {
 				jo = new JSONObject(lastCheckPoint);
@@ -161,6 +277,46 @@ public class FileDocumentList implements DocumentList {
 				throw new RepositoryException(
 						"JSON exception, while getting last checkpoint.", e);
 			}
+		} else {
+			logger.fine("date of the first push : " + dateFirstPush);
+			dateString = dateFirstPush;
+		}
+
+		// Document list to send delete feed as per the
+		logger.info("fileDocumentToDeleteDocs : " + fileDocumentToDeleteDocs);
+		logger.info("lastCheckPoint : " + lastCheckPoint);
+
+		if (fileDocumentToDeleteDocs != null) {
+			Property val = fetchAndVerifyValueForCheckpoint(fileDocumentToDeleteDocs, SpiConstants.PROPNAME_LASTMODIFIED);
+
+			try {
+				String dateStr = val.nextValue().toString();
+				dateStringDocumentToDeleteDocs = FileDateValue.calendarToIso8601(dateStr);
+			} catch (ParseException e1) {
+				logger.log(Level.WARNING, "Unable to parse the date string for add. Date string format may be incorrect.", e1);
+				throw new RepositoryException("Unexpected JSON problem", e1);
+			} catch (Exception e1) {
+				logger.log(Level.WARNING, "Unable to parse the date string for add. Date string format may be incorrect.", e1);
+				throw new RepositoryException("Unexpected JSON problem", e1);
+			}
+			logger.log(Level.FINE, "dateString of the checkpoint of deleted document is "
+					+ dateStringDocumentToDeleteDocs);
+
+		} else if (lastCheckPoint != null) {
+			logger.fine("Get the last modified date from the last checkpoint ");
+			JSONObject jo;
+			try {
+				jo = new JSONObject(lastCheckPoint);
+				docIdToDeleteDocs = jo.getString("uuidToDeleteDocs");
+				dateStringDocumentToDeleteDocs = jo.getString("lastModifiedDate");
+			} catch (JSONException e) {
+				logger.log(Level.WARNING, "JSON exception, while getting last checkpoint.", e);
+				throw new RepositoryException(
+						"JSON exception, while getting last checkpoint.", e);
+			}
+		} else {
+			logger.fine("date of the first push : " + dateFirstPush);
+			dateStringDocumentToDeleteDocs = dateFirstPush;
 		}
 
 		logger.info("fileDocumentToDelete : " + fileDocumentToDelete);
@@ -202,6 +358,8 @@ public class FileDocumentList implements DocumentList {
 			JSONObject jo = new JSONObject();
 			jo.put("uuid", docId);
 			jo.put("lastModified", dateString);
+			jo.put("uuidToDeleteDocs", docIdToDeleteDocs);
+			jo.put("lastModifiedDate", dateStringDocumentToDeleteDocs);
 			jo.put("uuidToDelete", docIdToDelete);
 			jo.put("lastRemoveDate", dateStringDocumentToDelete);
 			result = jo.toString();
--- changes/Dhanashri/FileNet 2.8.0 Changes/FilNet4_merge/fnapi-core/source/java/com/google/enterprise/connector/filenet4/filejavawrap/FnVersionSeries.java
+++ changes/Dhanashri/FileNet 2.8.0 Changes/FilNet4_merge/fnapi-core/source/java/com/google/enterprise/connector/filenet4/filejavawrap/FnVersionSeries.java
@@ -1,17 +1,19 @@
 package com.google.enterprise.connector.filenet4.filejavawrap;
 
-import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import com.filenet.api.core.Document;
-import com.filenet.api.core.VersionSeries;
-import com.filenet.api.util.Id;
 import com.google.enterprise.connector.filenet4.filewrap.IDocument;
 import com.google.enterprise.connector.filenet4.filewrap.IVersionSeries;
 import com.google.enterprise.connector.spi.RepositoryDocumentException;
 import com.google.enterprise.connector.spi.RepositoryException;
+import com.google.enterprise.connector.spi.SpiConstants;
 import com.google.enterprise.connector.spi.SpiConstants.ActionType;
+
+import com.filenet.api.core.Document;
+import com.filenet.api.core.VersionSeries;
+import com.filenet.api.util.Id;
+
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FnVersionSeries implements IVersionSeries {
 
@@ -32,7 +34,8 @@ public class FnVersionSeries implements IVersionSeries {
 
 	}
 
-	public Date getModifyDate(ActionType action) throws RepositoryDocumentException {
+	public Date getModifyDate(ActionType action)
+			throws RepositoryDocumentException {
 		Date ModifyDate = new Date();
 		return ModifyDate;
 	}
@@ -41,35 +44,42 @@ public class FnVersionSeries implements IVersionSeries {
 		return this.versionSeries.getClassName();
 	}
 
-	public Date getPropertyDateValueDelete(String name) throws RepositoryDocumentException {
+	public Date getPropertyDateValueDelete(String name)
+			throws RepositoryDocumentException {
 		return new Date();
 	}
 
-	public String getVersionSeriesId() throws RepositoryDocumentException {
+	public String getVersionSeriesId(ActionType action)
+			throws RepositoryDocumentException {
 		Id id;
 		String strId;
 		try {
-			id = ((com.filenet.apiimpl.core.DeletionEventImpl) versionSeries).get_VersionSeriesId();
-		}catch (Exception e){
+
+			if (SpiConstants.ActionType.DELETE.equals(action)) {
+				id = ((com.filenet.apiimpl.core.DeletionEventImpl) versionSeries).get_VersionSeriesId();
+			} else {// if action==SpiConstants.ActionType.ADD
+				id = ((com.filenet.apiimpl.core.DocumentImpl) versionSeries).get_ReleasedVersion().get_VersionSeries().get_Id();
+			}
+		} catch (Exception e) {
 			logger.log(Level.WARNING, "Unable to get the VersionSeriesId");
 			throw new RepositoryDocumentException(e);
 		}
-//		logger.info("versionId : ID : "+id);
-		strId=id.toString();
-//		logger.info("versionId : tostring : "+strId);
-		strId = strId.substring(1,strId.length()-1);
-//		logger.info("versionId : cut start/end : "+strId);
+		// logger.info("versionId : ID : "+id);
+		strId = id.toString();
+		// logger.info("versionId : tostring : "+strId);
+		strId = strId.substring(1, strId.length() - 1);
+		// logger.info("versionId : cut start/end : "+strId);
 		return strId;
 	}
 
 	public IDocument getCurrentVersion() throws RepositoryException {
-		return new FnDocument((Document) this.versionSeries
-				.get_CurrentVersion());
+		return new FnDocument(
+				(Document) this.versionSeries.get_CurrentVersion());
 	}
 
 	public IDocument getReleasedVersion() throws RepositoryException {
-		return new FnDocument((Document) this.versionSeries
-				.get_ReleasedVersion());
+		return new FnDocument(
+				(Document) this.versionSeries.get_ReleasedVersion());
 	}
 
 }
