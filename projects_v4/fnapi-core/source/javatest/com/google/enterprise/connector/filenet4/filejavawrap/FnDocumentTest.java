462c5e014ad7c13b4780350f16b7393216a7161e
John Lacey <jlacey@google.com>
2013-08-12 20:48:23 +0000

    Fix compiler warnings in the tests.
    Code review: https://codereview.appspot.com/12428043

--- projects_v4/fnapi-core/source/javatest/com/google/enterprise/connector/filenet4/filejavawrap/FnDocumentTest.java
+++ projects_v4/fnapi-core/source/javatest/com/google/enterprise/connector/filenet4/filejavawrap/FnDocumentTest.java
@@ -1,16 +1,17 @@
-// Copyright (C) 2007-2010 Google Inc.
+// Copyright 2007-2008 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package com.google.enterprise.connector.filenet4.filejavawrap;
 
 import com.google.enterprise.connector.filenet4.FileConnector;
@@ -28,6 +29,7 @@ import com.google.enterprise.connector.filenet4.filewrap.IVersionSeries;
 import com.google.enterprise.connector.spi.RepositoryException;
 import com.google.enterprise.connector.spi.RepositoryLoginException;
 import com.google.enterprise.connector.spi.SpiConstants.ActionType;
+import com.google.enterprise.connector.spi.Value;
 
 import com.filenet.api.constants.ClassNames;
 
@@ -77,7 +79,6 @@ public class FnDocumentTest extends FileNetTestCase {
     uc.authenticate(TestConnection.username, TestConnection.password);
     vs = fd.getVersionSeries();
     fd2 = vs.getReleasedVersion();
-
   }
 
   /*
@@ -85,10 +86,9 @@ public class FnDocumentTest extends FileNetTestCase {
    * 'com.google.enterprise.connector.file.filejavawrap.FnDocument.getPropertyName()'
    */
   public void ftestGetPropertyName() throws RepositoryException {
-
-    Set test = fd.getPropertyName();
+    Set<String> test = fd.getPropertyName();
     assertNotNull(test);
-    Set Test2 = TestConnection.included_meta;
+    Set<String> Test2 = TestConnection.included_meta;
     Test2.add("Id");
     Test2.add("ClassDescription");
     Test2.add("ContentElements");
@@ -100,21 +100,18 @@ public class FnDocumentTest extends FileNetTestCase {
     Test2.remove("LockTimeout");
     Test2.remove("LockToken");
     Test2.remove("StorageLocation");
-    Iterator it = Test2.iterator();
 
+    Iterator<String> it = Test2.iterator();
     while (it.hasNext()) {
-      String comp1 = it.next().toString();
+      String comp1 = it.next();
       String comp2 = null;
-      // System.out.println("reel : "+comp1);
-      Iterator it2 = test.iterator();
+      Iterator<String> it2 = test.iterator();
       while (it2.hasNext()) {
-        comp2 = it2.next().toString();
-        // System.out.println("test : "+comp2);
+        comp2 = it2.next();
         if (comp2.equals(comp1)) {
           break;
         }
       }
-      // System.out.println("==========");
       assertEquals(comp1, comp2);
     }
   }
@@ -124,19 +121,18 @@ public class FnDocumentTest extends FileNetTestCase {
    * 'com.google.enterprise.connector.file.filejavawrap.FnDocument.getPropertyType(String)'
    */
   public void ftestGetPropertyType() throws RepositoryException {
-
     String[][] typeArray = TestConnection.type;
 
-    Set meta = TestConnection.included_meta;
+    Set<String> meta = TestConnection.included_meta;
     meta.add("Id");
     meta.add("ClassDescription");
     meta.add("ContentElements");
     meta.add("DateLastModified");
     meta.add("MimeType");
     meta.add("VersionSeries");
-    Iterator metaIt = meta.iterator();
+    Iterator<String> metaIt = meta.iterator();
     while (metaIt.hasNext()) {
-      String property = metaIt.next().toString();
+      String property = metaIt.next();
       String typeGet = fd.getPropertyType(property);
       String typeSet = null;
       for (int i = 0; i < typeArray.length; i++) {
@@ -192,7 +188,7 @@ public class FnDocumentTest extends FileNetTestCase {
    * 'com.google.enterprise.connector.file.filejavawrap.FnDocument.getPropertyStringValue(String)'
    */
   public void testGetPropertyStringValue() throws RepositoryException {
-    LinkedList list = new LinkedList();
+    LinkedList<Value> list = new LinkedList<Value>();
     fd.getPropertyStringValue("Name", list);
     try {
       assertEquals("Either Metadata is not found or Metadata is MultiValued.", 1, list.size());
@@ -209,7 +205,7 @@ public class FnDocumentTest extends FileNetTestCase {
    * 'com.google.enterprise.connector.file.filejavawrap.FnDocument.getPropertyGuidValue(String)'
    */
   public void ftestGetPropertyGuidValue() throws RepositoryException {
-    LinkedList list = new LinkedList();
+    LinkedList<Value> list = new LinkedList<Value>();
     fd.getPropertyGuidValue("MyID", list);
     try {
       assertEquals("Either Metadata is not found or Metadata is MultiValued.", 1, list.size());
@@ -224,7 +220,7 @@ public class FnDocumentTest extends FileNetTestCase {
    * 'com.google.enterprise.connector.file.filejavawrap.FnDocument.getPropertyLongValue(String)'
    */
   public void ftestGetPropertyLongValue() throws RepositoryException {
-    LinkedList list = new LinkedList();
+    LinkedList<Value> list = new LinkedList<Value>();
     fd.getPropertyLongValue("MyInteger", list);
     try {
       assertEquals("Either Metadata is not found or Metadata is MultiValued.", 1, list.size());
@@ -239,7 +235,7 @@ public class FnDocumentTest extends FileNetTestCase {
    * 'com.google.enterprise.connector.file.filejavawrap.FnDocument.getPropertyDoubleValue(String)'
    */
   public void ftestGetPropertyDoubleValue() throws RepositoryException {
-    LinkedList list = new LinkedList();
+    LinkedList<Value> list = new LinkedList<Value>();
     fd.getPropertyDoubleValue("MyFloat", list);
     Double value = new Double(2998306.0);
     try {
@@ -255,7 +251,7 @@ public class FnDocumentTest extends FileNetTestCase {
    * 'com.google.enterprise.connector.file.filejavawrap.FnDocument.getPropertyDateValue(String)'
    */
   public void ftestGetPropertyDateValue() throws RepositoryException {
-    LinkedList list = new LinkedList();
+    LinkedList<Value> list = new LinkedList<Value>();
     fd.getPropertyDateValue("MyDate", list);
     Date testSetted = new Date(1183555393920L);
     try {
@@ -271,7 +267,7 @@ public class FnDocumentTest extends FileNetTestCase {
    * 'com.google.enterprise.connector.file.filejavawrap.FnDocument.getPropertyBooleanValue(String)'
    */
   public void ftestGetPropertyBooleanValue() throws RepositoryException {
-    LinkedList list = new LinkedList();
+    LinkedList<Value> list = new LinkedList<Value>();
     fd.getPropertyBooleanValue("MyBoolean", list);
     try {
       assertEquals("Either Metadata is not found or Metadata is MultiValued.", 1, list.size());
@@ -286,7 +282,7 @@ public class FnDocumentTest extends FileNetTestCase {
    * 'com.google.enterprise.connector.file.filejavawrap.FnDocument.getPropertyBinaryValue(String)'
    */
   public void ftestGetPropertyBinaryValue() throws RepositoryException {
-    LinkedList list = new LinkedList();
+    LinkedList<Value> list = new LinkedList<Value>();
     fd.getPropertyBinaryValue("MyBinary", list);
     try {
       assertEquals("Either Metadata is not found or Metadata is MultiValued.", 1, list.size());
--- projects_v4/fnapi-core/source/javatest/com/google/enterprise/connector/filenet4/filejavawrap/FnObjectFactoryTest.java
+++ projects_v4/fnapi-core/source/javatest/com/google/enterprise/connector/filenet4/filejavawrap/FnObjectFactoryTest.java
@@ -1,3 +1,17 @@
+// Copyright 2007-2008 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.enterprise.connector.filenet4.filejavawrap;
 
 import com.google.enterprise.connector.filenet4.FileConnector;
@@ -16,9 +30,9 @@ import com.google.enterprise.connector.spi.SpiConstants.ActionType;
 
 import com.filenet.api.util.UserContext;
 
-import java.util.Iterator;
-
 import junit.framework.TestCase;
+
+import java.util.Iterator;
 
 public class FnObjectFactoryTest extends TestCase {
 
@@ -82,9 +96,9 @@ public class FnObjectFactoryTest extends TestCase {
         + "and ContentSize IS NOT NULL  AND (ISCLASS(d, Document) " 
         + "OR ISCLASS(d, WorkflowDefinition))  ORDER BY DateLastModified,Id");
     assertEquals(1, test.getSize());
-    Iterator it = test.getIterator();
+    Iterator<? extends IBaseObject> it = test.getIterator();
     while (it.hasNext()) {
-      IBaseObject ibo = (IBaseObject) it.next();
+      IBaseObject ibo = it.next();
       assertEquals(TestConnection.docId1, ibo.getId(ActionType.ADD));
     }
   }
