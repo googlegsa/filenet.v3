35f3fd96e389be54363fcbbecfab5ae6a8a6f4fa
Thomas Nguyen <tdnguyen@google.com>
2014-03-21 21:30:11 +0000

    Disable direct ACLs for FileNet 3.2.4 release on branches/3.2.x
    Changes:
    - Remove ACL property lookup for users and groups.
    - Remove property names for ACL in FileDocument.getPropertyNames method.
    
    https://codereview.appspot.com/76960045/

--- projects_v4/fnapi-client/source/java/com/google/enterprise/connector/filenet4/FileDocument.java
+++ projects_v4/fnapi-client/source/java/com/google/enterprise/connector/filenet4/FileDocument.java
@@ -133,18 +133,19 @@ public class FileDocument implements Document {
         logger.fine("Getting Property " + name + " : "
                 + action.toString());
         return new SimpleProperty(list);
-      } else if (SpiConstants.PROPNAME_ACLUSERS.equals(name)) {
-        addPrincipals(list, name, document.getPermissions().getAllowUsers());
-        return new SimpleProperty(list);
-      } else if (SpiConstants.PROPNAME_ACLDENYUSERS.equals(name)) {
-        addPrincipals(list, name, document.getPermissions().getDenyUsers());
-        return new SimpleProperty(list);
-      } else if (SpiConstants.PROPNAME_ACLGROUPS.equals(name)) {
-        addPrincipals(list, name, document.getPermissions().getAllowGroups());
-        return new SimpleProperty(list);
-      } else if (SpiConstants.PROPNAME_ACLDENYGROUPS.equals(name)) {
-        addPrincipals(list, name, document.getPermissions().getDenyGroups());
-        return new SimpleProperty(list);
+      // Disable ACLs for the 3.2.4 release
+      // } else if (SpiConstants.PROPNAME_ACLUSERS.equals(name)) {
+      //   addPrincipals(list, name, document.getPermissions().getAllowUsers());
+      //   return new SimpleProperty(list);
+      // } else if (SpiConstants.PROPNAME_ACLDENYUSERS.equals(name)) {
+      //   addPrincipals(list, name, document.getPermissions().getDenyUsers());
+      //   return new SimpleProperty(list);
+      // } else if (SpiConstants.PROPNAME_ACLGROUPS.equals(name)) {
+      //   addPrincipals(list, name, document.getPermissions().getAllowGroups());
+      //        return new SimpleProperty(list);
+      // } else if (SpiConstants.PROPNAME_ACLDENYGROUPS.equals(name)) {
+      //   addPrincipals(list, name, document.getPermissions().getDenyGroups());
+      //   return new SimpleProperty(list);
       } else if (name.startsWith(SpiConstants.RESERVED_PROPNAME_PREFIX)) {
         return null;
       } else {
@@ -188,10 +189,11 @@ public class FileDocument implements Document {
       // return empty set of property names
       return properties;
     }
-    properties.add(SpiConstants.PROPNAME_ACLUSERS);
-    properties.add(SpiConstants.PROPNAME_ACLDENYUSERS);
-    properties.add(SpiConstants.PROPNAME_ACLGROUPS);
-    properties.add(SpiConstants.PROPNAME_ACLDENYGROUPS);
+    // Disable ACLs for the 3.2.4 release
+    // properties.add(SpiConstants.PROPNAME_ACLUSERS);
+    // properties.add(SpiConstants.PROPNAME_ACLDENYUSERS);
+    // properties.add(SpiConstants.PROPNAME_ACLGROUPS);
+    // properties.add(SpiConstants.PROPNAME_ACLDENYGROUPS);
 
     fetch();
     Set<String> documentProperties = document.getPropertyNames();
