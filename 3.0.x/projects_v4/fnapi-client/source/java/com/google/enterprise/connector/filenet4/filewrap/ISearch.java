fa5898ec1ac032ff166758053883787b8b461c7b
John Lacey <jlacey@google.com>
2014-04-02 02:34:35 +0000

    Remove the extra /tags/3.0.4/3.0.x directory created in r551. It is a duplicate of /tags/3.0.4 itself.

--- 3.0.x/projects_v4/fnapi-client/source/java/com/google/enterprise/connector/filenet4/filewrap/ISearch.java
+++
@@ -1,9 +0,0 @@
-package com.google.enterprise.connector.filenet4.filewrap;
-
-import com.google.enterprise.connector.spi.RepositoryException;
-
-public interface ISearch {
-
-  public IObjectSet execute(String query) throws RepositoryException;
-
-}
