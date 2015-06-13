googlegsa/filenet.v3

commit a9fa34b10f19a689219db7a2193b3f21cd89987d

Cleanup batch hint and SQL TOP clauses.
* Update default batch hint from 500 to 1000.
* Fix FileDocumentTraverser (which had a default of 0).
* Handle negative batch hints (which never happen) in
  FileTraversalManager by using 1000 instead.
* Remove checks for negative batch hints when adding TOP clause.
* Switch from StringBuffer to StringBuilder.

projects_v4/filenet/source/java/com/google/enterprise/connector/filenet4/SecurityFolderTraverser.java
@@ -49,14 +49,14 @@ class SecurityFolderTraverser implements Traverser {
       Logger.getLogger(SecurityFolderTraverser.class.getName());
 
   private static final String FOLDER_QUERY_WITH_UUID =
-      "SELECT {0} * FROM '" + GuidConstants.Class_Folder
+      "SELECT TOP {0} * FROM '" + GuidConstants.Class_Folder
       + "' WHERE ((" + PropertyNames.DATE_LAST_MODIFIED + " = {1}) AND ("
       + PropertyNames.ID + " > {2})) OR ("
       + PropertyNames.DATE_LAST_MODIFIED + " > {1}) ORDER BY "
       + PropertyNames.DATE_LAST_MODIFIED + "," + PropertyNames.ID;
 
   private static final String FOLDER_QUERY_WITHOUT_UUID =
-      "SELECT {0} * FROM '" + GuidConstants.Class_Folder
+      "SELECT TOP {0} * FROM '" + GuidConstants.Class_Folder
       + "' WHERE " + PropertyNames.DATE_LAST_MODIFIED + " > {1} ORDER BY "
       + PropertyNames.DATE_LAST_MODIFIED + "," + PropertyNames.ID;
 
@@ -64,7 +64,7 @@ class SecurityFolderTraverser implements Traverser {
   private final IObjectStore os;
   private final FileConnector connector;
 
-  private int batchHint = 500;
+  private int batchHint = 1000;
 
   public SecurityFolderTraverser(IObjectFactory objectFactory, IObjectStore os,
       FileConnector connector) {
@@ -80,7 +80,6 @@ class SecurityFolderTraverser implements Traverser {
   @Override
   public void setBatchHint(int batchHint) throws RepositoryException {
     this.batchHint = batchHint;
-    LOGGER.finest("Setting batchHint for FolderTraverser to " + batchHint);
   }
 
   @Override
@@ -175,14 +174,12 @@ class SecurityFolderTraverser implements Traverser {
     String checkpointUuid =
         getCheckpointValue(checkpoint, JsonField.UUID_FOLDER);
 
-    String topClause =
-        (batchHint > 0) ? "TOP " + batchHint : "";
     if (Strings.isNullOrEmpty(checkpointUuid)) {
       return MessageFormat.format(FOLDER_QUERY_WITHOUT_UUID,
-          new Object[] {topClause, timeStr});
+          new Object[] {batchHint, timeStr});
     } else {
       return MessageFormat.format(FOLDER_QUERY_WITH_UUID,
-          new Object[] {topClause, timeStr, checkpointUuid});
+          new Object[] {batchHint, timeStr, checkpointUuid});
     }
   }
 

projects_v4/filenet/source/java/com/google/enterprise/connector/filenet4/SecurityPolicyTraverser.java
@@ -57,14 +57,14 @@ class SecurityPolicyTraverser implements Traverser {
       Logger.getLogger(SecurityPolicyTraverser.class.getName());
 
   private static final String SEC_POLICY_QUERY_WITH_UUID =
-      "SELECT {0}* FROM '" + GuidConstants.Class_SecurityPolicy
+      "SELECT TOP {0} * FROM '" + GuidConstants.Class_SecurityPolicy
       + "' WHERE ((" + PropertyNames.DATE_LAST_MODIFIED + " = {1}) AND ("
       + PropertyNames.ID + " > {2})) OR ("
       + PropertyNames.DATE_LAST_MODIFIED + " > {1}) ORDER BY "
       + PropertyNames.DATE_LAST_MODIFIED + "," + PropertyNames.ID;
 
   private static final String SEC_POLICY_QUERY_WITHOUT_UUID =
-      "SELECT {0}* FROM '" + GuidConstants.Class_SecurityPolicy
+      "SELECT TOP {0} * FROM '" + GuidConstants.Class_SecurityPolicy
       + "' WHERE " + PropertyNames.DATE_LAST_MODIFIED + " > {1} ORDER BY "
       + PropertyNames.DATE_LAST_MODIFIED + "," + PropertyNames.ID;
 
@@ -77,7 +77,7 @@ class SecurityPolicyTraverser implements Traverser {
   private final IObjectStore os;
   private final FileConnector connector;
 
-  private int batchHint = 500;
+  private int batchHint = 1000;
 
   public SecurityPolicyTraverser(IObjectFactory objectFactory, IObjectStore os,
       FileConnector connector) {
@@ -195,16 +195,12 @@ class SecurityPolicyTraverser implements Traverser {
     String uuid =
         getCheckpointValue(checkpoint, JsonField.UUID_SECURITY_POLICY);
 
-    String topStr = "";
-    if (batchHint > 0) {
-      topStr = "TOP " + String.valueOf(batchHint) + " ";
-    }
     if (Strings.isNullOrEmpty(uuid)) {
       return MessageFormat.format(SEC_POLICY_QUERY_WITHOUT_UUID,
-          new Object[] {topStr, timeStr});
+          new Object[] {batchHint, timeStr});
     } else {
       return MessageFormat.format(SEC_POLICY_QUERY_WITH_UUID,
-          new Object[] {topStr, timeStr, uuid});
+          new Object[] {batchHint, timeStr, uuid});
     }
   }
 

projects_v4/filenet/source/java/com/google/enterprise/connector/filenet4/FileDocumentTraverser.java
@@ -85,7 +85,7 @@ public class FileDocumentTraverser implements Traverser {
   private final FileConnector connector;
 
   private TraversalContext traversalContext;
-  private int batchHint;
+  private int batchHint = 1000;
 
   public FileDocumentTraverser(IObjectFactory fileObjectFactory,
       IObjectStore objectStore, FileConnector fileConnector) {
@@ -168,10 +168,9 @@ public class FileDocumentTraverser implements Traverser {
    */
   private String buildQueryString(Checkpoint checkpoint)
           throws RepositoryException {
-    StringBuffer query = new StringBuffer("SELECT ");
-    if (batchHint > 0) {
-      query.append("TOP " + batchHint + " ");
-    }
+    StringBuilder query = new StringBuilder("SELECT TOP ");
+    query.append(batchHint);
+    query.append(" ");
     query.append(PropertyNames.ID);
     query.append(",");
     query.append(PropertyNames.DATE_LAST_MODIFIED);
@@ -184,7 +183,7 @@ public class FileDocumentTraverser implements Traverser {
     String additionalWhereClause = connector.getAdditionalWhereClause();
     if (additionalWhereClause != null && !additionalWhereClause.equals("")) {
       if ((additionalWhereClause.toUpperCase()).startsWith("SELECT ID,DATELASTMODIFIED FROM ")) {
-        query = new StringBuffer(additionalWhereClause);
+        query = new StringBuilder(additionalWhereClause);
         query.replace(0, 6, "SELECT TOP " + batchHint + " ");
         LOGGER.fine("Using Custom Query[" + additionalWhereClause
                 + "]");
@@ -206,10 +205,9 @@ public class FileDocumentTraverser implements Traverser {
    */
   private String buildQueryStringToDeleteDocs(Checkpoint checkpoint,
       String deleteadditionalWhereClause) throws RepositoryException {
-    StringBuffer query = new StringBuffer("SELECT ");
-    if (batchHint > 0) {
-      query.append("TOP " + batchHint + " ");
-    }
+    StringBuilder query = new StringBuilder("SELECT TOP ");
+    query.append(batchHint);
+    query.append(" ");
     query.append(PropertyNames.ID);
     query.append(",");
     query.append(PropertyNames.DATE_LAST_MODIFIED);
@@ -218,7 +216,7 @@ public class FileDocumentTraverser implements Traverser {
     query.append(" WHERE VersionStatus=1 and ContentSize IS NOT NULL ");
 
     if ((deleteadditionalWhereClause.toUpperCase()).startsWith("SELECT ID,DATELASTMODIFIED FROM ")) {
-      query = new StringBuffer(deleteadditionalWhereClause);
+      query = new StringBuilder(deleteadditionalWhereClause);
       query.replace(0, 6, "SELECT TOP " + batchHint + " ");
       LOGGER.fine("Using Custom Query[" + deleteadditionalWhereClause + "]");
     } else {
@@ -248,12 +246,10 @@ public class FileDocumentTraverser implements Traverser {
   private String buildQueryToDelete(Checkpoint checkpoint)
           throws RepositoryException {
     LOGGER.fine("Build query to get the documents removed from repository: ");
-    StringBuffer query = new StringBuffer("SELECT ");
-    if (batchHint > 0) {
-      query.append("TOP ");
-      query.append(batchHint);
-      query.append(" ");
-    }
+    StringBuilder query = new StringBuilder("SELECT TOP ");
+    query.append(batchHint);
+    query.append(" ");
+
     // GuidConstants.Class_DeletionEvent = Only deleted objects in event
     // table
     query.append(PropertyNames.ID);

projects_v4/filenet/source/java/com/google/enterprise/connector/filenet4/FileTraversalManager.java
@@ -48,6 +48,11 @@ public class FileTraversalManager implements TraversalManager,
 
   @Override
   public void setBatchHint(int batchHint) throws RepositoryException {
+    // Reset the default if we get an unusable value.
+    if (batchHint <= 0) {
+      batchHint = 1000;
+    }
+    LOGGER.finest("Setting batchHint to " + batchHint);
     for (Traverser t : traversers) {
       t.setBatchHint(batchHint);
     }

commit 0f79d158bc2a0d554abc9556c238ad7cbf00b76f

Avoid locale formatting in SQL TOP clauses.
This was broken in commit a9fa34b.

Code review: https://codereview.appspot.com/243290043/

projects_v4/filenet/source/java/com/google/enterprise/connector/filenet4/SecurityFolderTraverser.java
@@ -50,14 +50,14 @@ class SecurityFolderTraverser implements Traverser {
       Logger.getLogger(SecurityFolderTraverser.class.getName());
 
   private static final String FOLDER_QUERY_WITH_UUID =
-      "SELECT TOP {0} * FROM '" + GuidConstants.Class_Folder
+      "SELECT TOP {0,number,#} * FROM '" + GuidConstants.Class_Folder
       + "' WHERE ((" + PropertyNames.DATE_LAST_MODIFIED + " = {1}) AND ("
       + PropertyNames.ID + " > {2})) OR ("
       + PropertyNames.DATE_LAST_MODIFIED + " > {1}) ORDER BY "
       + PropertyNames.DATE_LAST_MODIFIED + "," + PropertyNames.ID;
 
   private static final String FOLDER_QUERY_WITHOUT_UUID =
-      "SELECT TOP {0} * FROM '" + GuidConstants.Class_Folder
+      "SELECT TOP {0,number,#} * FROM '" + GuidConstants.Class_Folder
       + "' WHERE " + PropertyNames.DATE_LAST_MODIFIED + " > {1} ORDER BY "
       + PropertyNames.DATE_LAST_MODIFIED + "," + PropertyNames.ID;
 

projects_v4/filenet/source/java/com/google/enterprise/connector/filenet4/SecurityPolicyTraverser.java
@@ -54,14 +54,14 @@ class SecurityPolicyTraverser implements Traverser {
       Logger.getLogger(SecurityPolicyTraverser.class.getName());
 
   private static final String SEC_POLICY_QUERY_WITH_UUID =
-      "SELECT TOP {0} * FROM '" + GuidConstants.Class_SecurityPolicy
+      "SELECT TOP {0,number,#} * FROM '" + GuidConstants.Class_SecurityPolicy
       + "' WHERE ((" + PropertyNames.DATE_LAST_MODIFIED + " = {1}) AND ("
       + PropertyNames.ID + " > {2})) OR ("
       + PropertyNames.DATE_LAST_MODIFIED + " > {1}) ORDER BY "
       + PropertyNames.DATE_LAST_MODIFIED + "," + PropertyNames.ID;
 
   private static final String SEC_POLICY_QUERY_WITHOUT_UUID =
-      "SELECT TOP {0} * FROM '" + GuidConstants.Class_SecurityPolicy
+      "SELECT TOP {0,number,#} * FROM '" + GuidConstants.Class_SecurityPolicy
       + "' WHERE " + PropertyNames.DATE_LAST_MODIFIED + " > {1} ORDER BY "
       + PropertyNames.DATE_LAST_MODIFIED + "," + PropertyNames.ID;
 