797c93bb269148c76a6d905a58f16e46cd3f1b5c
Srinivas Veldurthi <srinicodebytes@gmail.com>
2016-03-01 10:27:53 -0800

    FileNet Group Lookup.  Dumb down identity user domain.  Add test cases.
    Code reiew: https://codereview.appspot.com/288160043/

--- projects_v4/filenet/source/java/com/google/enterprise/connector/filenet4/FileAuthenticationManager.java
+++ projects_v4/filenet/source/java/com/google/enterprise/connector/filenet4/FileAuthenticationManager.java
@@ -14,6 +14,7 @@
 
 package com.google.enterprise.connector.filenet4;
 
+import com.google.common.base.Strings;
 import com.google.enterprise.connector.filenet4.api.IConnection;
 import com.google.enterprise.connector.filenet4.api.IUserContext;
 import com.google.enterprise.connector.spi.AuthenticationIdentity;
@@ -36,12 +37,11 @@ import java.util.logging.Logger;
 
 /**
  * FileNet Authentication Manager. It contains method for authenticating the user while search is performed.
- * @author pankaj_chouhan
- * */
+ */
 public class FileAuthenticationManager implements AuthenticationManager {
   private static final Logger logger =
       Logger.getLogger(FileAuthenticationManager.class.getName());
-  
+
   private final IConnection conn;
   private final String globalNamespace;
   private final boolean returnGroups;
@@ -56,13 +56,26 @@ public class FileAuthenticationManager implements AuthenticationManager {
   /**
    * authenticates the user
    * @param authenticationIdentity: contains user credentials
-   * */
+   */
   @Override
   public AuthenticationResponse authenticate(AuthenticationIdentity id)
       throws RepositoryException {
     IUserContext uc = conn.getUserContext();
     try {
-      User user = uc.authenticate(id.getUsername(), id.getPassword());
+      User user;
+      if (Strings.isNullOrEmpty(id.getPassword())) {
+        user = uc.lookupUser(id.getUsername());
+      } else {
+        user = uc.authenticate(id.getUsername(), id.getPassword());
+      }
+      String identityDomain = getDomain(id);
+      if (!Strings.isNullOrEmpty(identityDomain)
+          && !matchesDomain(identityDomain, user.get_Email(),
+              user.get_Name(), user.get_DistinguishedName())) {
+        logger.log(Level.FINE, "GSA identity {0} does not match {1}.",
+            new Object[] {id, user});
+        return new AuthenticationResponse(false, "");
+      }
       if (returnGroups) {
         List<Principal> principalGroups = FileUtil.getPrincipals(
             PrincipalType.UNKNOWN, globalNamespace, getGroupNames(user),
@@ -74,9 +87,71 @@ public class FileAuthenticationManager implements AuthenticationManager {
       } else {
         return new AuthenticationResponse(true, "");
       }
-    } catch (Throwable e) {
+    } catch (RuntimeException e) {
       logger.log(Level.WARNING, "Authentication failed for user " + id, e);
       return new AuthenticationResponse(false, "");
+    }
+  }
+
+  /**
+   * Returns domain from AuthenticationIdentity.
+   */
+  private String getDomain(AuthenticationIdentity id) {
+    String user = id.getUsername();
+    String identityDomain = id.getDomain();
+    String domain = null;
+    if (Strings.isNullOrEmpty(identityDomain)) {
+      if (!Strings.isNullOrEmpty(user)) {
+        domain = parseDomain(user);
+      }
+    } else {
+      domain = identityDomain;
+    }
+    return domain;
+  }
+
+  private boolean matchesDomain(String identityDomain, String... names) {
+    boolean identityHasFqdn = (identityDomain.indexOf('.') > -1);
+    for (String name : names) {
+      if (!Strings.isNullOrEmpty(name)) {
+        String userDomain = parseDomain(FileUtil.convertDn(name));
+        if (identityHasFqdn && (userDomain.indexOf('.') > -1)) {
+          if (identityDomain.equalsIgnoreCase(userDomain)) {
+            return true;
+          }
+        } else {
+          if (dumbDown(identityDomain).equalsIgnoreCase(dumbDown(userDomain))) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Parses the domain from name which is in following formats: username@domain,
+   * domain\\username or domain/username.
+   *
+   * @param string name
+   * @return string domain
+   */
+  private String parseDomain(String name) {
+    if (name.indexOf('@') > -1) {
+      String[] ary = name.split("@");
+      return ary[ary.length - 1];
+    } else {
+      String[] ary = name.split("\\\\");
+      return ary[0];
+    }
+  }
+
+  private String dumbDown(String domain) {
+    int pos = domain.indexOf('.');
+    if (pos == -1) {
+      return domain;
+    } else {
+      return domain.substring(0, pos);
     }
   }
 
--- projects_v4/filenet/source/javatests/com/google/enterprise/connector/filenet4/FileAuthenticationManagerTest.java
+++ projects_v4/filenet/source/javatests/com/google/enterprise/connector/filenet4/FileAuthenticationManagerTest.java
@@ -13,45 +13,57 @@
 // limitations under the License.
 package com.google.enterprise.connector.filenet4;
 
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.isA;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.enterprise.connector.filenet4.EngineCollectionMocks.GroupSetMock;
+import com.google.enterprise.connector.filenet4.api.IConnection;
+import com.google.enterprise.connector.filenet4.api.IUserContext;
+import com.google.enterprise.connector.spi.AuthenticationIdentity;
 import com.google.enterprise.connector.spi.AuthenticationManager;
 import com.google.enterprise.connector.spi.AuthenticationResponse;
 import com.google.enterprise.connector.spi.Principal;
 import com.google.enterprise.connector.spi.RepositoryException;
 import com.google.enterprise.connector.spi.SimpleAuthenticationIdentity;
 
-import org.junit.Before;
+import com.filenet.api.security.Group;
+import com.filenet.api.security.User;
+
 import org.junit.Test;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class FileAuthenticationManagerTest {
-
-  @Before
-  public void checkLiveConnection() throws Exception {
-    assumeTrue(TestConnection.isLiveConnection());
-  }
-
   private AuthenticationManager getObjectUnderTest(boolean pushAcls)
       throws RepositoryException {
-    FileConnector connec = new FileConnector();
-    connec.setUsername(TestConnection.adminUsername);
-    connec.setPassword(TestConnection.adminPassword);
-    connec.setObject_store(TestConnection.objectStore);
-    connec.setWorkplace_display_url(TestConnection.displayURL);
-    connec.setObject_factory(TestConnection.objectFactory);
-    connec.setContent_engine_url(TestConnection.uri);
+    FileConnector connec = TestObjectFactory.newFileConnector();
     connec.setPushAcls(pushAcls);
 
     return connec.login().getAuthenticationManager();
   }
 
+  private AuthenticationManager getObjectUnderTest(IConnection conn)
+      throws RepositoryException {
+    return new FileAuthenticationManager(conn, null, true);
+  }
+
   @Test
   public void testAuthenticate() throws RepositoryException {
+    assumeTrue(TestConnection.isLiveConnection());
+
     AuthenticationManager fatm = getObjectUnderTest(true);
     SimpleAuthenticationIdentity fai = new SimpleAuthenticationIdentity(
         TestConnection.username, TestConnection.password);
@@ -77,6 +89,8 @@ public class FileAuthenticationManagerTest {
   @Test
   public void testAuthenticateNoGroupsIfPushAclsFalse()
       throws RepositoryException {
+    assumeTrue(TestConnection.isLiveConnection());
+
     AuthenticationManager fatm = getObjectUnderTest(false);
     SimpleAuthenticationIdentity fai = new SimpleAuthenticationIdentity(
         TestConnection.username, TestConnection.password);
@@ -86,10 +100,308 @@ public class FileAuthenticationManagerTest {
   }
 
   @Test
-  public void testAuthenticate_fail() throws RepositoryException  {
+  public void testAuthenticate_fail() throws RepositoryException {
+    assumeTrue(TestConnection.isLiveConnection());
+
     AuthenticationManager fatm = getObjectUnderTest(true);
     SimpleAuthenticationIdentity faiWrong = new SimpleAuthenticationIdentity(TestConnection.username, TestConnection.wrongPassword);
     AuthenticationResponse arWrong = fatm.authenticate(faiWrong);
     assertEquals(false, arWrong.isValid());
+  }
+
+  private User getMockUserNoReplay(String email, String name, String dn) {
+    User user = createMock(User.class);
+    expect(user.get_Email()).andReturn(email);
+    expect(user.get_Name()).andReturn(name);
+    expect(user.get_DistinguishedName()).andReturn(dn);
+    return user;
+  }
+
+  private User getMockUser(String email, String name, String dn) {
+    User user = getMockUserNoReplay(email, name, dn);
+    replay(user);
+    return user;
+  }
+
+  private User getMockUserWithGroup(String email, String name, String dn) {
+    Group group = createMock(Group.class);
+    expect(group.get_Name()).andReturn(
+        "cn=Group1,cn=Groups,dc=example,dc=com");
+    User user = getMockUserNoReplay(email, name, dn);
+    expect(user.get_MemberOfGroups()).andReturn(
+        new GroupSetMock(Collections.singletonList(group)));
+    replay(group, user);
+    return user;
+  }
+
+  private SimpleAuthenticationIdentity getIdentityUser(String identityUser,
+      String identityDomain) {
+    return new SimpleAuthenticationIdentity(identityUser, null, identityDomain);
+  }
+
+  private AuthenticationResponse testGroupLookup(
+      AuthenticationIdentity id, User user, boolean expectAuthenticated)
+      throws RepositoryException {
+    IUserContext uc = createMock(IUserContext.class);
+    expect(uc.lookupUser(isA(String.class))).andReturn(user);
+    IConnection conn = createMock(IConnection.class);
+    expect(conn.getUserContext()).andReturn(uc);
+    replay(uc, conn);
+
+    AuthenticationManager fam = getObjectUnderTest(conn);
+    AuthenticationResponse authResp = fam.authenticate(id);
+    assertNotNull(authResp);
+    assertEquals(expectAuthenticated, authResp.isValid());
+    verify(uc, conn);
+
+    return authResp;
+  }
+
+  @Test
+  public void testGroupLookup_matchUserEmail() throws RepositoryException {
+    // Expects an exact match of domain in ID with domain in user's email
+    testGroupLookup(
+        getIdentityUser("jsmith", "example.com"),
+        getMockUserWithGroup("jsmith@example.com", "jsmith",
+            "cn=jsmith,ou=Users,dc=example,dc=com"),
+        true);
+  }
+
+  @Test
+  public void testGroupLookup_matchUserName() throws RepositoryException {
+    // Expects a match of domain in ID with domain in user's name
+    testGroupLookup(
+        getIdentityUser("jsmith", "example.com"),
+        getMockUserWithGroup("jsmith@foo.example.com", "jsmith@example.com",
+            "cn=jsmith,ou=Users,dc=foo,dc=example,dc=com"),
+        true);
+  }
+
+  @Test
+  public void testGroupLookup_matchUserDn() throws RepositoryException {
+    // Expects a match of domain in ID with domain in user's DN
+    testGroupLookup(
+        getIdentityUser("jsmith", "example.com"),
+        getMockUserWithGroup("jsmith@foo.example.com", "jsmith",
+            "cn=jsmith,ou=Users,dc=example,dc=com"),
+        true);
+  }
+
+  @Test
+  public void testGroupLookup_matchUserNetbiosName()
+      throws RepositoryException {
+    // Expects a match of domain in ID with dumb-down domain in user's name
+    testGroupLookup(
+        getIdentityUser("jsmith", "example"),
+        getMockUserWithGroup("jsmith@foo.example.com", "example\\jsmith",
+            "cn=jsmith,ou=Users,dc=foo,dc=com"),
+        true);
+  }
+
+  @Test
+  public void testGroupLookup_matchDumbdownUserEmail()
+      throws RepositoryException {
+    // Expects a match of domain in ID with dumb-down email domain
+    testGroupLookup(
+        getIdentityUser("jsmith", "example"),
+        getMockUserWithGroup("jsmith@example.com", "foo\\jsmith",
+            "cn=jsmith,ou=Users,dc=foo,dc=example,dc=com"), true);
+  }
+
+  @Test
+  public void testGroupLookup_matchUserDumbdownUserDn()
+      throws RepositoryException {
+    // Expects a match of domain in ID with dumb-down domain in user's DN
+    testGroupLookup(
+        getIdentityUser("jsmith", "example"),
+        getMockUserWithGroup("jsmith@foo.example.com", "foo\\jsmith",
+            "cn=jsmith,ou=Users,dc=example,dc=com"),
+        true);
+  }
+
+  @Test
+  public void testGroupLookup_invalidDnsDomain() throws RepositoryException {
+    testGroupLookup(
+        getIdentityUser("jsmith", "example.com"),
+        getMockUser("jsmith@example.com.au", "foo\\jsmith",
+            "cn=jsmith,ou=Users,dc=foo,dc=example,dc=com"),
+        false);
+  }
+
+  @Test
+  public void testGroupLookup_invalidNetbiosDomain()
+      throws RepositoryException {
+    testGroupLookup(
+        getIdentityUser("jsmith", "example"),
+        getMockUser("jsmith@foo.example.com", "foo\\jsmith",
+            "cn=jsmith,ou=Users,dc=foo,dc=example,dc=com"),
+        false);
+  }
+
+  private Set<String> getPrincipalNames(Collection<?> coll) {
+    ImmutableSet.Builder<String> builder = new ImmutableSet.Builder<String>();
+    Iterator<?> iterator = coll.iterator();
+    while (iterator.hasNext()) {
+      Principal group = (Principal) iterator.next();
+      builder.add(group.getName());
+    }
+    return builder.build();
+  }
+
+  @Test
+  public void testGroupLookup_groups() throws RepositoryException {
+    AuthenticationResponse authResp =
+        testGroupLookup(
+            getIdentityUser("jsmith", "foo.example.com"),
+            getMockUserWithGroup("", "",
+                "cn=jsmith,ou=Users,dc=foo,dc=example,dc=com"),
+            true);
+    assertEquals(
+        ImmutableSet.of("Group1@example.com", Permissions.AUTHENTICATED_USERS),
+        getPrincipalNames(authResp.getGroups()));
+  }
+
+  @Test
+  public void testAuthenUser_dnsShortWrong() throws RepositoryException {
+    testGroupLookup(
+        getIdentityUser("jsmith", "example"),
+        getMockUserWithGroup("", "",
+            "cn=jsmith,ou=Users,dc=foo,dc=example,dc=com"),
+        false);
+  }
+
+  @Test
+  public void testAuthenUser_noDomain() throws RepositoryException {
+    testGroupLookup(
+        getIdentityUser("jsmith@foo.example.com", ""),
+        getMockUserWithGroup("", "",
+            "cn=jsmith,ou=Users,dc=foo,dc=example,dc=com"),
+        true);
+  }
+
+  @Test
+  public void testAuthenUser_noDomainDnsUserAussieMockUser()
+      throws RepositoryException {
+    testGroupLookup(
+        getIdentityUser("jsmith@example.com", ""),
+        getMockUser("jsmith@example.com.au", "foo\\jsmith",
+            "cn=jsmith,ou=Users,dc=foo,dc=example,dc=com"),
+        false);
+  }
+
+  @Test
+  public void testAuthenUser_noDomainWindowsUser() throws RepositoryException {
+    testGroupLookup(
+        getIdentityUser("foo\\jsmith", ""),
+        getMockUserWithGroup("", "",
+            "cn=jsmith,ou=Users,dc=foo,dc=example,dc=com"),
+        true);
+  }
+
+  @Test
+  public void testAuthenUser_noDomainWrongWindowsUser()
+      throws RepositoryException {
+    testGroupLookup(
+        getIdentityUser("somedomain\\jsmith", ""),
+        getMockUserWithGroup("", "",
+            "cn=jsmith,ou=Users,dc=foo,dc=example,dc=com"),
+        false);
+  }
+
+  @Test
+  public void testAuthenUser_domainWindowsUserAussieMockUser()
+      throws RepositoryException {
+    testGroupLookup(
+        getIdentityUser("example.com.au\\jsmith", ""),
+        getMockUserWithGroup("jsmith@example.com.au", "foo\\jsmith",
+            "cn=jsmith,ou=Users,dc=foo,dc=example,dc=com"),
+        true);
+  }
+
+  @Test
+  public void testAuthenUser_noDomainWindowsUserAussieMockUser()
+      throws RepositoryException {
+    testGroupLookup(
+        getIdentityUser("example.com\\jsmith", ""),
+        getMockUser("jsmith@example.com.au", "foo\\jsmith",
+            "cn=jsmith,ou=Users,dc=foo,dc=example,dc=com"),
+        false);
+  }
+
+  @Test
+  public void testGroupLookup_repositoryException() throws RepositoryException {
+    IUserContext uc = createMock(IUserContext.class);
+    expect(uc.lookupUser(isA(String.class))).andThrow(
+        new RepositoryException("User not found"));
+    IConnection conn = createMock(IConnection.class);
+    expect(conn.getUserContext()).andReturn(uc);
+    replay(uc, conn);
+
+    SimpleAuthenticationIdentity id =
+        new SimpleAuthenticationIdentity(TestConnection.username);
+    AuthenticationManager fam = getObjectUnderTest(conn);
+
+    try {
+      AuthenticationResponse authResp = fam.authenticate(id);
+    } catch (RepositoryException expected) {
+    }
+    verify(uc, conn);
+  }
+
+  @Test
+  public void testGroupLookup_runtimeException() throws RepositoryException {
+    IUserContext uc = createMock(IUserContext.class);
+    expect(uc.lookupUser(isA(String.class))).andThrow(
+        new RuntimeException("User not found"));
+    IConnection conn = createMock(IConnection.class);
+    expect(conn.getUserContext()).andReturn(uc);
+    replay(uc, conn);
+
+    SimpleAuthenticationIdentity id =
+        new SimpleAuthenticationIdentity(TestConnection.username);
+    AuthenticationManager fam = getObjectUnderTest(conn);
+
+    AuthenticationResponse authResp = fam.authenticate(id);
+    assertNotNull(authResp);
+    assertEquals(false, authResp.isValid());
+    verify(uc, conn);
+  }
+
+  @Test
+  public void testGroupLookup_liveShortname() throws RepositoryException {
+    assumeTrue(TestConnection.isLiveConnection());
+
+    testGroupLookup_live(TestConnection.username);
+  }
+
+  @Test
+  public void testGroupLookup_liveShortnameDomain()
+      throws RepositoryException {
+    assumeTrue(TestConnection.isLiveConnection());
+
+    testGroupLookup_live(TestConnection.username + "@" + TestConnection.domain);
+  }
+
+  private void testGroupLookup_live(String username)
+      throws RepositoryException {
+    AuthenticationManager fatm = getObjectUnderTest(true);
+    SimpleAuthenticationIdentity id =
+        new SimpleAuthenticationIdentity(TestConnection.username);
+    AuthenticationResponse authResp = fatm.authenticate(id);
+    assertNotNull(TestConnection.username + " failed authentication", authResp);
+    assertTrue("No group is found for " + TestConnection.username,
+        authResp.getGroups().size() > 1);
+  }
+
+  @Test
+  public void testGroupLookup_liveFakeUser() throws RepositoryException {
+    assumeTrue(TestConnection.isLiveConnection());
+
+    AuthenticationManager fatm = getObjectUnderTest(true);
+    SimpleAuthenticationIdentity id =
+        new SimpleAuthenticationIdentity("fake_user");
+    AuthenticationResponse authResp = fatm.authenticate(id);
+    assertNotNull("Authentication response is not returned", authResp);
+    assertEquals(false, authResp.isValid());
   }
 }
